3357613fec1253d9fb73c54ce0af16e9498c0470
GroovieGermanikus <groovie@mango.markets>
2023-05-14 20:11:17 +0200

    add CompletionState

--- src/bin/parse_pcap.rs
+++ src/bin/parse_pcap.rs
@@ -22,7 +22,7 @@ use tokio::sync::watch::Sender;
 use tokio::task::JoinHandle;
 use tokio::time;
 use tokio::time::Interval;
-
+use crate::CompletionState::{Complete, DataCompleteNotYetSeen, MissingDataByIndex, MissingDataBySize};
 
 
 fn main() {
@@ -86,10 +86,10 @@ fn main() {
 
 }
 
+
+
 fn process_all_shreds(all_shreds: Vec<Shred>) {
     println!("processing {} shreds", all_shreds.len());
-    let reed_solomon_cache = ReedSolomonCache::default();
-
 
 
     let counts = all_shreds.iter().map(|s| (s.slot(), s.fec_set_index()) ).counts();
@@ -111,122 +111,108 @@ fn process_all_shreds(all_shreds: Vec<Shred>) {
 
         println!("selected {} shreds for slot {}", only_my_slot.len(), my_slot);
 
-        //
-        // recovery looks into first shred to get the slot which then is asserted to be the same for all other shreds
-        // match solana_ledger::shred::recover(vec![only_my_slot.first().unwrap().clone()], &reed_solomon_cache) {
-
-        // return `Error::TooFewShardsPresent` when there are not enough shards for reconstruction.
-        let recovered = match solana_ledger::shred::recover(only_my_slot.clone(), &reed_solomon_cache) {
-            Ok(recovered_shreds) => {
-                println!("recovered {:?} shreds from {}", recovered_shreds.len(), only_my_slot.len());
-                recovered_shreds
-            }
-            Err(err) => {
-                println!("recover2 error {:?}", err);
-                vec![]
-            }
-        };
+        shreds_for_slot_and_fecindex(my_slot, only_my_slot);
 
 
-
-        let mut collector: Vec<u8> = Vec::new();
-        let mut indizes_seen: HashSet<u32> = HashSet::new();
-        let mut last_in_slot_index = None;
-
-        only_my_slot
-            .iter()
-            .chain(recovered.iter())
-            .sorted_by_key(|s| s.index())
-            .for_each(|s| {
-                // let mut hasher = Hasher::default();
-                // hasher.hash(&s.bytes_to_store());
-                // println!("index {} hash {} (is_data={})", s.index(), hasher.result(), s.is_data());
-
-                // println!("shred: {:?}", s);
-
-                // TODO check vs s.last_in_slot()
-                if s.data_complete() {
-                    last_in_slot_index = Some(s.index());
-                }
-
-                match s {
-                    Shred::ShredData(_) => {
-                        collector.extend_from_slice(s.bytes_to_store());
-
-                        indizes_seen.insert(s.index());
-
-                        // let Shred::ShredData(daaaata) = s else { todo!(); };
-                        // println!("daaaata {:?}", daaaata);
-                    }
-                    Shred::ShredCode(_) => {
-                        collector.extend_from_slice(s.bytes_to_store());
-                        indizes_seen.insert(s.index());
-                    }
-                }
-
-            });
-
-        println!("total data so far {}", collector.len());
-
-        match last_in_slot_index {
-            None => {
-                println!("no complete packet seen for slot {}", my_slot);
-            }
-            Some(last) => {
-                println!("last = {}", last);
-                println!("indizes_seen(sorted) {:?}", indizes_seen.iter().sorted().collect_vec());
-                let complete = check_if_complete(&indizes_seen, last);
-                println!("completed {:?}", complete);
-            }
-        }
-
-        // indizes_seen.sort();
-
-        println!("indizes_seen {:?}", indizes_seen);
-
-
-            // .map(|s| {
-            //     let mut hasher = Hasher::default();
-            //     hasher.hash(&s.bytes_to_store());
-            //     println!("index {} hash {} (is_data={})", s.index(), hasher.result(), s.is_data());
-            //     s
-            // })
-            // .dedup()
-            // .filter_map(|shred| match shred {
-            //     Shred::ShredCode(_) => None,
-            //     Shred::ShredData(shred) => Some(shred),
-            // })
-            // .for_each(|shred| {
-            //     println!("shred {:?}", shred);
-            //     collector.append(shred);
-            // });
-        // Assert that the input data can be recovered from data shreds.
-
-
-
-
-
-    } // -- for slots
-
-    // TODO may need to check DATA_COMPLETE_SHRED
-
-    // // The the max slot
-    // let my_slot = winner.0.0;
-    // let fec_index = winner.0.1;
-
-
-
-    // println!("selected {} shreds for slot {}", only_my_slot.len(), my_slot);
+     } // -- for slots
 
 }
 
-fn check_if_complete(all_seen: &HashSet<u32>, last_index: u32) -> bool {
+fn shreds_for_slot_and_fecindex(my_slot: &Slot, only_my_slot: Vec<Shred>) {
+    let reed_solomon_cache = ReedSolomonCache::default();
 
-    if all_seen.len() != 1 + last_index as usize {
-        return false;
+
+    // recovery looks into first shred to get the slot which then is asserted to be the same for all other shreds
+    // match solana_ledger::shred::recover(vec![only_my_slot.first().unwrap().clone()], &reed_solomon_cache) {
+
+    // return `Error::TooFewShardsPresent` when there are not enough shards for reconstruction.
+    let recovered = match solana_ledger::shred::recover(only_my_slot.clone(), &reed_solomon_cache) {
+        Ok(recovered_shreds) => {
+            println!("recovered {:?} shreds from {}", recovered_shreds.len(), only_my_slot.len());
+            recovered_shreds
+        }
+        Err(err) => {
+            println!("recover2 error {:?}", err);
+            vec![]
+        }
+    };
+
+
+    let mut collector: Vec<u8> = Vec::new();
+    let mut indizes_seen: HashSet<u32> = HashSet::new();
+    let mut last_in_slot_index = None;
+
+    only_my_slot
+        .iter()
+        .chain(recovered.iter())
+        .sorted_by_key(|s| s.index())
+        .for_each(|s| {
+            // let mut hasher = Hasher::default();
+            // hasher.hash(&s.bytes_to_store());
+            // println!("index {} hash {} (is_data={})", s.index(), hasher.result(), s.is_data());
+
+            // println!("shred: {:?}", s);
+
+            // TODO check vs s.last_in_slot()
+            if s.data_complete() {
+                last_in_slot_index = Some(s.index());
+            }
+
+            match s {
+                Shred::ShredData(_) => {
+                    collector.extend_from_slice(s.bytes_to_store());
+
+                    indizes_seen.insert(s.index());
+
+                    // let Shred::ShredData(daaaata) = s else { todo!(); };
+                    // println!("daaaata {:?}", daaaata);
+                }
+                Shred::ShredCode(_) => {
+                    collector.extend_from_slice(s.bytes_to_store());
+                    indizes_seen.insert(s.index());
+                }
+            }
+        });
+
+    println!("total data so far {}", collector.len());
+
+    println!("last = {last_in_slot_index:?}");
+    println!("indizes_seen(sorted) {:?}", indizes_seen.iter().sorted().collect_vec());
+    let complete = check_if_complete(&indizes_seen, last_in_slot_index);
+    println!("completed status {:?}", complete);
+
+    // indizes_seen.sort();
+
+    println!("indizes_seen {:?}", indizes_seen);
+}
+
+#[derive(Debug)]
+enum CompletionState {
+    Complete(u32), // OK
+    DataCompleteNotYetSeen(u32),
+    // check for size of index array failed
+    MissingDataBySize(u32,u32), // (seen, last_index) - note: last_index is rangge-inclusive
+    // check if all indices are there failed
+    MissingDataByIndex,
+
+}
+fn check_if_complete(all_seen: &HashSet<u32>, last_in_slot_index: Option<u32>) -> CompletionState {
+
+    match last_in_slot_index {
+        None => DataCompleteNotYetSeen(all_seen.len() as u32),
+        Some(last_index) => {
+
+            if all_seen.len() != 1 + last_index as usize {
+                return MissingDataBySize(all_seen.len() as u32, last_index)
+            }
+
+            if !(0..=last_index).all(|i| all_seen.contains(&i)) {
+                return MissingDataByIndex; // TODO provide missing indices
+            }
+
+            return Complete(last_index);
+        }
     }
-
-    return (0..=last_index).all(|i| all_seen.contains(&i));
 }
 
 
