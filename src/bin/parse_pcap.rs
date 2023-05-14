580b7185313c6b5474e69fca49ef214279a27880
GroovieGermanikus <groovie@mango.markets>
2023-05-14 18:59:32 +0200

    rebuild most of a block

--- src/bin/parse_pcap.rs
+++ src/bin/parse_pcap.rs
@@ -3,6 +3,7 @@ use pcap_parser::*;
 use pcap_parser::traits::PcapReaderIterator;
 use std::fs::File;
 use std::net::{SocketAddr, UdpSocket};
+use std::ops::{Deref, Range, RangeTo};
 use etherparse::IpNumber::Udp;
 use etherparse::{SlicedPacket, TransportSlice};
 
@@ -12,8 +13,9 @@ use std::sync::atomic::Ordering::Relaxed;
 use std::time::Duration;
 use itertools::Itertools;
 use serde::{Deserialize, Serialize};
-use solana_ledger::shred::{Error, ReedSolomonCache, Shred};
+use solana_ledger::shred::{Error, ReedSolomonCache, Shred, ShredData};
 use solana_sdk::clock::Slot;
+use solana_sdk::hash::Hasher;
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio::sync::watch::Sender;
 use tokio::task::JoinHandle;
@@ -83,21 +85,21 @@ fn main() {
 
 }
 
-fn process_all_shreds(shreds: Vec<Shred>) {
-    println!("processing {} shreds", shreds.len());
+fn process_all_shreds(all_shreds: Vec<Shred>) {
+    println!("processing {} shreds", all_shreds.len());
     let reed_solomon_cache = ReedSolomonCache::default();
 
 
 
-    let counts = shreds.iter().map(|s| (s.slot(), s.fec_set_index()) ).counts();
+    let counts = all_shreds.iter().map(|s| (s.slot(), s.fec_set_index()) ).counts();
     let winner = counts.iter().max_by_key((|s| s.1)).unwrap();
     println!("counts {:?}", counts);
     println!("winner {:?}", winner);
 
     for ((my_slot, fec_index),cnt) in counts.iter() {
-        println!("slot {} {} count {}", my_slot, fec_index, cnt);
+        println!("slot {} {} count {} ...", my_slot, fec_index, cnt);
 
-        let only_my_slot = shreds.iter()
+        let only_my_slot = all_shreds.iter()
             .map(|s| {
                 // println!("slot {}", s.slot());
                 // println!("index {}", s.index());
@@ -114,17 +116,99 @@ fn process_all_shreds(shreds: Vec<Shred>) {
         // match solana_ledger::shred::recover(vec![only_my_slot.first().unwrap().clone()], &reed_solomon_cache) {
 
         // return `Error::TooFewShardsPresent` when there are not enough shards for reconstruction.
-        match solana_ledger::shred::recover(only_my_slot.clone(), &reed_solomon_cache) {
+        let recovered = match solana_ledger::shred::recover(only_my_slot.clone(), &reed_solomon_cache) {
             Ok(recovered_shreds) => {
                 println!("recovered {:?} shreds from {}", recovered_shreds.len(), only_my_slot.len());
+                recovered_shreds
             }
             Err(err) => {
                 println!("recover2 error {:?}", err);
+                vec![]
+            }
+        };
+
+
+
+        let mut collector: Vec<u8> = Vec::new();
+        let mut indizes_seen: Vec<u32> = Vec::new();
+        let mut last_in_slot_index = None;
+
+        only_my_slot
+            .iter()
+            .chain(recovered.iter())
+            .sorted_by_key(|s| s.index())
+            .for_each(|s| {
+                // let mut hasher = Hasher::default();
+                // hasher.hash(&s.bytes_to_store());
+                // println!("index {} hash {} (is_data={})", s.index(), hasher.result(), s.is_data());
+
+                // println!("shred: {:?}", s);
+
+                // TODO check vs s.last_in_slot()
+                if s.data_complete() {
+                    last_in_slot_index = Some(s.index());
+                }
+
+                match s {
+                    Shred::ShredData(_) => {
+                        collector.extend_from_slice(s.bytes_to_store());
+                        indizes_seen.push(s.index());
+
+                        // let Shred::ShredData(daaaata) = s else { todo!(); };
+                        // println!("daaaata {:?}", daaaata);
+                    }
+                    Shred::ShredCode(_) => {
+                        collector.extend_from_slice(s.bytes_to_store());
+                        indizes_seen.push(s.index());
+                    }
+                }
+
+            });
+
+        println!("total data so far {}", collector.len());
+
+        match last_in_slot_index {
+            None => {
+                println!("no complete packet seen for slot {}", my_slot);
+            }
+            Some(last) => {
+                println!("last = {}", last);
+                indizes_seen.sort();
+                println!("indizes_seen {:?}", indizes_seen);
+                let complete = check_if_complete(&indizes_seen, last);
+                println!("completed {:?}", complete);
             }
         }
 
+        // indizes_seen.sort();
 
-    }
+        println!("indizes_seen {:?}", indizes_seen);
+
+
+            // .map(|s| {
+            //     let mut hasher = Hasher::default();
+            //     hasher.hash(&s.bytes_to_store());
+            //     println!("index {} hash {} (is_data={})", s.index(), hasher.result(), s.is_data());
+            //     s
+            // })
+            // .dedup()
+            // .filter_map(|shred| match shred {
+            //     Shred::ShredCode(_) => None,
+            //     Shred::ShredData(shred) => Some(shred),
+            // })
+            // .for_each(|shred| {
+            //     println!("shred {:?}", shred);
+            //     collector.append(shred);
+            // });
+        // Assert that the input data can be recovered from data shreds.
+
+
+
+
+
+    } // -- for slots
+
+    // TODO may need to check DATA_COMPLETE_SHRED
 
     // // The the max slot
     // let my_slot = winner.0.0;
@@ -134,6 +218,16 @@ fn process_all_shreds(shreds: Vec<Shred>) {
 
     // println!("selected {} shreds for slot {}", only_my_slot.len(), my_slot);
 
+}
+
+fn check_if_complete(all_seen: &Vec<u32>, last_index: u32) -> bool {
+
+    if all_seen.len() != last_index as usize {
+        return false;
+    }
+
+
+    return (0..last_index).all(|i| all_seen.contains(&i));
 }
 
 
