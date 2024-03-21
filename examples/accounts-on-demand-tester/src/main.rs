7c6c398814cae84c0f39f90156d278bef74bd21a
GroovieGermanikus <groovie@mango.markets>
2024-03-21 09:16:43 +0100

    correlate error

--- examples/accounts-on-demand-tester/src/main.rs
+++ examples/accounts-on-demand-tester/src/main.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::Arc;
+use std::sync::atomic::AtomicU32;
 use futures::future::join_all;
 use log::info;
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
+use solana_rpc_client_api::client_error::ErrorKind;
+use solana_rpc_client_api::request::RpcError;
 use solana_sdk::commitment_config::CommitmentConfig;
 use solana_sdk::pubkey::Pubkey;
 
@@ -24,10 +28,12 @@ pub async fn main() {
 
     for account_chunk in accounts.chunks(5) {
         let mut account_fns = vec![];
-        for account in account_chunk {
+        let mut account_pks = vec![];
+        for account_pk in account_chunk {
             for _i in 0..3 {
-                let fun = rpc_client.get_account_with_commitment(&account, CommitmentConfig::processed());
+                let fun = rpc_client.get_account_with_commitment(&account_pk, CommitmentConfig::processed());
                 account_fns.push(fun);
+                account_pks.push(account_pk);
             }
         }
 
@@ -36,16 +42,54 @@ pub async fn main() {
         let ok_count = results.iter().filter(|x| x.is_ok()).count();
         let err_count = results.iter().filter(|x| x.is_err()).count();
 
+        let zipped = account_pks.iter().zip(results.iter());
+
         info!("accounts OK: {:?} ERR: {:?}", ok_count, err_count);
 
-        for ref res in results {
+        let mut errors = HashMap::<Pubkey, AtomicU32>::with_capacity(1000);
+        for (account_pk, ref res) in zipped {
             match res {
-                Ok(_) => {}
+                Ok(rpc_repsonse) => {
+                    info!("success account: {:?}", account_pk)
+                }
                 Err(res_err) => {
+
+                    if let ErrorKind::RpcError(RpcError::ForUser(for_user)) = &res_err.kind {
+                        info!("ForUser: {:?}", for_user);
+                        let pubkey = parse_pubkey(for_user);
+                        info!("pubkey: {:?}", pubkey);
+                        errors.entry(pubkey)
+                            .and_modify(|counter| { counter.fetch_add(1, std::sync::atomic::Ordering::Relaxed); } )
+                            .or_insert(AtomicU32::new(0));
+                    }
                     info!("ERROR account: {:?}", res_err);
                 }
             }
         }
     }
 
+}
+
+fn parse_pubkey(input: &str) -> Pubkey {
+    let split: Vec<&str> = input.split(":").collect();
+    let split = split[1].trim();
+    let split = split.replace("pubkey=", "");
+    Pubkey::from_str(&split).unwrap()
+}
+
+#[test]
+fn parser_foruser_error() {
+    let input = "AccountNotFound: pubkey=95oWcswpsjjoeWcWFZzpAQ1CzsAiYur5PwYbs8SxxZCG: error sending request for url (http://127.0.0.1:8890/): error trying to connect: tcp connect error: Connection refused (os error 61)";
+    let split: Vec<&str> = input.split(":").collect();
+    let split = split[1].trim();
+    assert_eq!(
+        split,
+        "pubkey=95oWcswpsjjoeWcWFZzpAQ1CzsAiYur5PwYbs8SxxZCG"
+    );
+    let split = split.replace("pubkey=", "");
+    assert_eq!(
+        split,
+        "95oWcswpsjjoeWcWFZzpAQ1CzsAiYur5PwYbs8SxxZCG"
+    );
+    assert_eq!(split, parse_pubkey(&input).to_string());
 }
